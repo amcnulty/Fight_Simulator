e30b5250ef4ff6872cc370f1daf897beff61b41a
amcnulty <amcnulty88@swbell.net>
2017-09-19 15:10:38 -0700

    Title Screen
    Added a title screen with star wars style crawling text.

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -7,6 +7,12 @@ if (window.attachEvent) {window.attachEvent('onload', load);}
 else if (window.addEventListener) {window.addEventListener('load', load, false);}
 else {document.addEventListener('load', load, false);}
 function load() {
-    alert("Hello Star Wars Game");
+    var $title = $("#title");
+
+    (function() {
+        // alert("Window Height: " + $(window).height() + "\nDocument Height: " + $(document).height());
+        // $title.css({bottom: -$title.height() + "px", transform: "rotateX(20deg) translateZ(0)"});
+        // $title.animate({bottom: $(window).height(), transform: "rotateX(25deg) translateZ(-2500px)"}, 3000, "linear");
+    })();
 
 }
