bc90a383b81bd33c120d4a39163739ecec903a17
amcnulty <amcnulty88@swbell.net>
2017-09-19 12:05:11 -0700

    Hello Star Wars
    Set up the html, css, and js.
    Added favicon.ico icon image.
    Added a custom star wars font.
    Program prints hello star wars.

---
+++ assets/javascript/game.js
@@ -0,0 +1,12 @@
+// Star Wars Fight Simulator 2017
+// By: Aaron Michael McNulty
+// Monkey Stomp Games 2017
+//
+// All rights reserved
+if (window.attachEvent) {window.attachEvent('onload', load);}
+else if (window.addEventListener) {window.addEventListener('load', load, false);}
+else {document.addEventListener('load', load, false);}
+function load() {
+    alert("Hello Star Wars Game");
+
+}
